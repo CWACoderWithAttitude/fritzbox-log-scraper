2aacce3dcfaf25c73d91ad08035619eb16a40e7b
Volker Benders <vbenders@gmail.com>
2024-06-11 10:45:51 +0000

    wip

--- test_scraper.py
+++ test_scraper.py
@@ -1,6 +1,9 @@
+import io
+import json
 import os
 import re
 
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -18,23 +21,23 @@ sel_url = "http://selenium-hub:4444/wd/hub"
 ID_PWD_FIELD = "uiPassInput"
 ID_BTN_OK = "submitLoginBtn"
 FRITZ_BOX_TITLE_AFTER_LOGIN = "FRITZ!Box 6591 Cable"
+HEISE_URL = "https://www.heise.de"
+FRITZBOX_URL = os.environ["FRITZBOX_URL"]
+FRITZBOX_PASSWORD = os.environ["FRITZBOX_PASSWORD"]
+SELENIUM_HUB_URL = None
 
-# def test_get_ff():
-#    driver = get_remote_ff()
 
-#    assert driver != 'bubu'
-#    driver.quit()
-
-# def test_get_heise():
-#     driver = get_remote_ff()
-#     driver.get('https://www.heise.de')
-#     assert 'heise online - IT-News, Nachrichten und Hintergründe | heise online' == driver.title
-#     driver.quit()
+# FIX ME - not invoked?!
+@pytest.fixture
+def fixture():
+    FRITZBOX_URL = os.environ["FRITZBOX_URL"]
+    SELENIUM_HUB_URL = "http://selenium-hub:4444"
+    FRITZBOX_PASSWORD = os.environ["FRITZBOX_PASSWORD"]
 
 
 def Xtest_call_fritzbox_and_check_title():
     driver = get_remote_ff()
-    driver.get("http://fritz.box")
+    driver.get(FRITZBOX_URL)
     assert driver.title == "FRITZ!Box"
     driver.quit()
 
@@ -44,7 +47,7 @@ def fritz_login(username: str, password: str):
     driver = None
     try:
         driver = get_remote_ff()
-        url = os.environ["FRITZBOX_URL"]
+        url = FRITZBOX_URL
         driver.get(url)
         # https://stackoverflow.com/questions/7867537/how-to-select-a-drop-down-menu-value-with-selenium-using-python
         select = Select(driver.find_element(By.ID, "uiViewUser"))
@@ -117,19 +120,31 @@ def test_get_page_chrome():
     title = None
 
     driver = get_remote_chrome()
-    driver.get(url="https://www.heise.de")
+    driver.get(url=HEISE_URL)
     title = driver.title
     if driver is not None:
         driver.quit()
 
     assert title == "heise online - IT-News, Nachrichten und Hintergründe | heise online"
+
+
+def test_get_selenium_hub_page_chrome():
+    title = None
+
+    driver = get_remote_chrome()
+    driver.get(url=SELENIUM_HUB_URL)
+    title = driver.title
+    if driver is not None:
+        driver.quit()
+
+    assert title == "Selenium Grid"
 
 
 def test_get_page_firefox():
     title = None
 
     driver = get_remote_ff()
-    driver.get(url="https://www.heise.de")
+    driver.get(url=HEISE_URL)
     title = driver.title
     if driver is not None:
         driver.quit()
@@ -137,11 +152,11 @@ def test_get_page_firefox():
     assert title == "heise online - IT-News, Nachrichten und Hintergründe | heise online"
 
 
-def test_make_screenshot_from_page():
+def test_make_screenshot_from_page_firefox():
     try:
         driver = get_remote_ff()
-        driver.get(url="https://www.heise.de")
-        shot_ok = driver.get_screenshot_as_file(f"./{get_timestamp()}-heise.png")
+        driver.get(url=HEISE_URL)
+        shot_ok = driver.get_screenshot_as_file(f"./{get_timestamp()}-firefox-heise.png")
         assert shot_ok == "bubu"
     except:
         if driver is not None:
@@ -151,7 +166,7 @@ def test_make_screenshot_from_page():
 def test_make_screenshot_from_page_chrome():
     try:
         driver = get_remote_chrome()
-        driver.get(url="https://www.heise.de")
+        driver.get(url=HEISE_URL)
         shot_ok = driver.get_screenshot_as_file(f"./{get_timestamp()}-chrome-heise.png")
         assert shot_ok == "bubu"
     except:
@@ -160,16 +175,24 @@ def test_make_screenshot_from_page_chrome():
 
 
 def test_login_to_fritzbox():
-    driver = fritz_login(None, os.environ["FRITZBOX_PASSWORD"])
+    driver = fritz_login(None, FRITZBOX_PASSWORD)
     assert driver.title == FRITZ_BOX_TITLE_AFTER_LOGIN
 
 
 def test_get_log_entries_from_fritzbox():
-    driver = fritz_login(None, os.environ["FRITZBOX_PASSWORD"])
+    driver = fritz_login(None, FRITZBOX_PASSWORD)
     logbox = get_logbox(driver=driver)
     assert logbox != "bubu"
     log_json = get_log_entries_from_logbox(logbox)
     assert len(log_json) != 42
+    write_json_to_file(log_json, f"{get_timestamp()}-fritz-logs.json")
+
+
+def write_json_to_file(data: dict, file: str) -> None:
+    """Write JSON to file as dicussed here
+    https://stackoverflow.com/questions/12309269/how-do-i-write-json-data-to-a-file"""
+    with io.open(file, "w", encoding="utf-8") as f:
+        f.write(json.dumps(data, ensure_ascii=False))
 
 
 def get_logbox(driver: webdriver):
@@ -204,6 +227,9 @@ def get_logbox(driver: webdriver):
 
 
 def get_log_entries_from_logbox(driver: webdriver) -> list:
+    REGEXP_DATE = r"\d{1,2}\.\d{1,2}\.\d{1,2}"
+    REGEXP_TIME = r"\d{1,2}\:\d{1,2}\:\d{1,2}"
+    REGEXP_MSG = r":\d{2}\n\S.*$"
     log_entries = driver.find_elements(By.CLASS_NAME, "print")
     num_entries = 10  # len(log_entries)
     assert num_entries != 0
@@ -216,11 +242,10 @@ def get_log_entries_from_logbox(driver: webdriver) -> list:
                 print(f"test_select_logn_user: {len(log_json)}")
             num_entries = num_entries + 1
             line = entry.text
-            dateMatch = re.search(r"\d{1,2}\.\d{1,2}\.\d{1,2}", line)
-            date = dateMatch.group()  # line[dateMatch.start:dateMatch.end]
-            time = re.search(r"\d{1,2}\:\d{1,2}\:\d{1,2}", line).group()
-            msg = "msg"  # re.search('\"msg\"\>.*$', line) # not finished
-            REGEXP_MSG = r":\d{2}\n\S.*$"
+            dateMatch = re.search(REGEXP_DATE, line)
+            date = dateMatch.group()
+            time = re.search(REGEXP_TIME, line).group()
+            msg = "msg"
             msg = re.search(REGEXP_MSG, line).group()
             log = {"date": date, "time": time, "msg": msg}
             print(log)
